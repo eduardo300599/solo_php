8a444c08a1ef1dec7ae3524fc6f9df030f5150c5
Eduardo Williams <eduardowilliams300599@gmail.com>
2021-07-19 22:34:33 -0600

    aqui hago una funcion para sumar dos numeros

---
+++ hola/ejerciciosDeLibro/ejerciciosFaciles/03_tercero.php
@@ -0,0 +1,14 @@
+<?php
+
+function suma($n1, $n2)
+{
+    $resultado = $n1+$n2;
+
+    return $resultado;
+}
+
+
+echo suma(8,5);
+
+
+
--- hola/todo/01-array/probar.php
+++ hola/todo/01-array/probar.php
@@ -18,3 +18,4 @@ if(isset($_POST['correo']) || isset($_POST['contrasenia']))
 
 
 
+
